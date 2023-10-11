4f4d59a1b57d33e1334b403e1995a5278c5eab95
faraan-estiam <147595360+faraan-estiam@users.noreply.github.com>
2023-10-11 15:48:46 +0200

    added comments

--- main.py
+++ main.py
@@ -16,6 +16,18 @@ api = FastAPI(
 
 api.include_router(routers.router_students.router)
 
-#'Students' auront des 'Attendances' pour des 'Sessions'
-# utilisateurs, lien vers une ressource
-# API vendu à des centre de formations ... 'Center' -> Sessions + Students -> Attendences
+# Reste à faire 
+# X Sortir mon student's router dans un dossier "routers"
+# X Rédiger une documentation et l'ajouter à mon app FastAPI()
+# X Sortir mes pydantic models dans un dossier classes
+# X Ajouter les tags 
+
+
+# - et description pour chaque endpoing/methods
+# - En ajouter suivant votre projet
+
+
+# Spécification...
+# "Students" auront des "Attendances" pour des "Sessions"
+# Utilisateurs, lien vers une ressource
+# API vendu à des centre de formations ... "Center" -> Sessions + Students -> Attendances
