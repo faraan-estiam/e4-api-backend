72b9bc101d27d98fb9095106f098c7c0c6ebb8bb
faraan-estiam <147595360+faraan-estiam@users.noreply.github.com>
2023-10-18 10:58:35 +0200

    firebase initialize app

---
+++ database/firebase.py
@@ -0,0 +1,7 @@
+import firebase_admin
+from configs.firebase_config import firebaseConfig
+import pyrebase
+
+cred = firebase_admin.credentials.Certificate("configs/my-api-99db8-firebase-adminsdk-qebp2-445fda4e3e.json")
+firebase_admin.initialize_app(cred)
+firebase = pyrebase.initialize_app(firebaseConfig)
