c5572c11f9abca1e72a6af8b2ffc3014b74ae652
faraan-estiam <147595360+faraan-estiam@users.noreply.github.com>
2023-10-25 13:05:21 +0200

    stripe_usage edited

--- routers/router_stripe.py
+++ routers/router_stripe.py
@@ -67,10 +67,9 @@ async def webhook_received(request:Request, stripe_signature: str = Header(None)
     else : print(f'Unhandled event: {event_type}')
 
 @router.get('/usage')
-async def stripe_usage(user_data: int = Depends(get_current_user)):
-    firebase_user = auth.get_user(user_data['uid'])
-    stripe_data = db.child('users').child(firebase_user['uid']).child('stripe').get().val()
-    item_id = stripe_data(['item_id'])
+async def stripe_usage(userData: int = Depends(get_current_user)):
+    fireBase_user= auth.get_user(userData['uid'])
+    stripe_data= db.child("users").child(fireBase_user.uid).child("stripe").get().val()
     cust_id = stripe_data["cust_id"]
     return stripe.Invoice.upcoming(customer=cust_id)
 
