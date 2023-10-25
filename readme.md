f66fb77f31354067f20bf33ca85366aa854dba10
faraan-estiam <147595360+faraan-estiam@users.noreply.github.com>
2023-10-25 11:00:21 +0200

    added stripe webhook route and stripe configs

--- readme.md
+++ readme.md
@@ -1,7 +1,17 @@
 # ATTENTION POUR LANCER LE PROJET !
 Le router de l'api s'appelle `api` et non `app` !<br>
 Pour lancer l'application utilisez :
-```
+```Sh
 uvicorn main:api --reload
 ```
 Pensez également à ajouter votre dossier `configs/` et de modifier le fichier `database/firebase.py` en conséquence.
+
+Ajoutez un fichier `stripe_config.py` dans le dossier configs et le remplir de la façon suivante :
+```Python
+stripe_config = {
+    'public_key' : '_votre_clé_publique_',
+    'secret_key' : '_votre_clé_privée',
+    'price_id' : '_votre_price_id_',
+    'webhook_secret' : '_votre_webhook_secret_'
+}
+```
--- routers/router_stripe.py
+++ routers/router_stripe.py
@@ -1,4 +1,5 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Header, Request
+from configs.stripe_config import stripe_config
 import stripe
 
 router = APIRouter(
@@ -6,7 +7,10 @@ router = APIRouter(
     tags=['Stripe']
 )
 
-YOUR_DOMAIN = 'hhtp://localhost'
+#votre test secret API Key
+stripe.api_key = stripe_config['secret_key']
+
+YOUR_DOMAIN = 'http://localhost'
 
 @router.post('/checkout')
 async def stripe_checkout():
@@ -15,14 +19,35 @@ async def stripe_checkout():
             line_items=[
                 {
                     # PRICE ID DU PRODUIT QUE VOUS VOULEZ VENDRE
-                    'price': 'price_1O51yYAGH7tUuIcUbKRqqIpu',
+                    'price': stripe_config['price_id'],
                     'quantity': 1
                 }
             ],
-            mode = 'payment',
+            mode = 'subscription',
+            payment_method_types = ['Card'],
             succes_url = YOUR_DOMAIN+'/succes.html',
             cancel_url = YOUR_DOMAIN+'/cancel.html'
         )
         return checkout_session
     except Exception as e:
         return str(e)
+    
+@router.post('/webhook')
+async def webhook_received(request:Request, stripe_signature: str = Header(None)):
+    webhook_secret = stripe_config['webhook_secret']
+    data = await request.body
+    try:
+        event = stripe.Webhook.construct_event(
+            payload=data,
+            sig_header=stripe_signature,
+            secret=webhook_secret
+        )
+        event_data = event['data']
+    except Exception as e:
+        return {'error':str(e)}
+    
+    event_type = event ['type']
+    if event_type == 'checkout.session.completed': print('Checkout session completed')
+    elif event_type == 'invoice.paid': print('Invoice paid')
+    elif event_type == 'invoice.payment_failed': print('Invoice payment failed')
+    else : print(f'Unhandled event: {event_type}')
